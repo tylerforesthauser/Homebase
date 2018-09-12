6685b61848ccf7532fd8aa516e085b2e4d45a077
d8ahazard <donate.to.digitalhigh@gmail.com>
2018-09-12 11:48:16 -0500

    SUPERCHARGE IT!
    Oh boy...I sure am subtle. Let's see here:
    Add a simplified version of my util script for common PHP functions, logging, certs, etc.
    Add default cacert for CURL requests, because people like to do silly things with https.
    Add a /rw directory for config files.
    Add a sample config file.
    Add a log directory, placeholders.
    Fix script loading in index.html.
    Add a handy-dandy JsonXmlElement class in case we want to parse Plex XML to JSON easily.
    Update plex, tautulli api files to use util methods, simplify some things, add logging, checks for invalid values, etc.

--- plex-api.php
+++ plex-api.php
@@ -1,40 +1,37 @@
 <?php
+require_once "./util.php";
+scriptDefaults();
+$section = "PLEX";
+$configFile = dirname(__FILE__) . "/rw/config.ini.php";
+$result = "Please configure plex parameters.";
 
-// For example: http://localhost:32400
-define('PLEX_URL', 'http://localhost:32400');
+if (file_exists($configFile)) {
+	$data = parse_ini_file($configFile,true);
+	write_log("Got me a config: ".json_encode($data));
+	$url = $data["$section"]["URL"] ?? false;
+	$token = $data["$section"]["TOKEN"] ?? false;
 
-// tautulli/settings#tab_tabs-plex_media_server
-define('PLEX_TOKEN', '');
+	if ($url && $token) {
+		$url .= $_POST['postData'];
+		$header = [
+			'X-Plex-Platform: Web Server',
+			'X-Plex-Platform-Version: 1.0',
+			'X-Plex-Provides: controller',
+			'X-Plex-Client-Identifier: 923A3BBB-98AF-53CD-8916-D72BE92DA7E4',
+			'X-Plex-Product: Homebase (for Plex)',
+			'X-Plex-Version: 1.0',
+			'X-Plex-Device: Web Server',
+			'X-Plex-Device-Name: Homebase Web Server',
+			'X-Plex-Token: ' . $token,
+			'Accept: application/json',
+		];
+		$result = curlGet($url,$header);
+		write_log("Result: " . json_encode($result));
+	} else {
+		write_log("No ". ($url ? " token" : " url").".","ERROR");
+	}
+}
 
-$post = $_POST['postData'];
-
-$defaults = array(
-  CURLOPT_URL 		        => PLEX_URL . $post,
-  CURLOPT_AUTOREFERER     => true,
-  CURLOPT_RETURNTRANSFER  => true,
-  CURLOPT_CONNECTTIMEOUT  => 30,
-  CURLOPT_TIMEOUT         => 30,
-  CURLOPT_FAILONERROR     => true,
-  CURLOPT_FOLLOWLOCATION  => true,
-  CURLOPT_HTTPHEADER      => [
-    'X-Plex-Platform: Web Server',
-    'X-Plex-Platform-Version: 1.0',
-    'X-Plex-Provides: controller',
-    'X-Plex-Client-Identifier: 923A3BBB-98AF-53CD-8916-D72BE92DA7E4',
-    'X-Plex-Product: Homebase (for Plex)',
-    'X-Plex-Version: 1.0',
-    'X-Plex-Device: Web Server',
-    'X-Plex-Device-Name: Homebase Web Server',
-    'X-Plex-Token: ' . PLEX_TOKEN,
-  	'Accept: application/json',
-  ],
-);
-
-$ch = curl_init();
-curl_setopt_array($ch, $defaults);
-$resultstr = curl_exec($ch);
-curl_close($ch);
-
-echo $resultstr;
+echo $result;
 
 // EOF
